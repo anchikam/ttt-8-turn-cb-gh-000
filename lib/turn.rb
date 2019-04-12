bc112d7a6fbdff8888a7ec936b86fdd2c3c7c214
Anna Manukian <amanu001@odu.edu>
2019-04-12 16:27:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,33 @@ def display_board(board=[" "," "," "," "," "," "," "," "," "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  b = board[index]
+  if index >= 9 or index < 0
+    false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
+  end
+end
+
+def position_taken?(board, index)
+  b = board[index]
+  if b == " " or b == "" or b == nil
+    false
+  elsif b == "X" or b == "O"
+    true
+  end
+end
+
+def input_to_index(n)
+  n = n.to_i - 1
+end
+
+def move(array, index, token="X")
+  array[index] = token
+end
 
 def turn
 
